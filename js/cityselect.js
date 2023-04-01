f2cf9ade40a9db41d08d0422691de453c3d2bf69
Keanu <113492880+Li-Keanu@users.noreply.github.com>
2023-04-01 08:32:55 +0800

    Delete js directory

--- js/cityselect.js
+++
@@ -1,8 +0,0 @@
-const lis = document.querySelectorAll('.content .nav li');
-
-for(let i = 0; i < lis.length; i++) {
-    lis[i].addEventListener('click', function() {
-        document.querySelector('.content .nav .select').classList.remove('select');
-        this.classList.add('select');
-    })
-}
